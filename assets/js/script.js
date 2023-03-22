dcd1635f2bc2fe8f0ad40ee8c099819a29a72865
Kyle Chart <kyliechart@yahoo.com>
2023-03-22 15:06:44 +0000

    Add function work flow in JS file. Add Docstrings to functions

---
+++ assets/js/script.js
@@ -0,0 +1,38 @@
+const startButton = document.getElementById('start-btn');
+const sounds = document.getElementById('sounds');
+
+/**
+ * Starts a new game when the quiz is loaded and the button is pressed
+ */
+function startNewGame() {
+
+}
+
+/**
+ * Sets the next question in the quiz, regardless of a correct answer
+ * or wrong answer
+ */
+function nextQuestion() {
+
+}
+
+/**
+ * Checks the answer to see if it is correct or wrong
+ */
+function selectAnswer() {
+
+}
+
+/**
+ * Controls the sounds by turning them on or off for the quiz
+ */
+function soundControl() {
+
+}
+
+/**
+ * Changes the stats of the quiz score and timer
+ */
+function stats() {
+
+}
